ba6b21755145644d8a8f2b906554293a2fddbf02
vasifalasgarzade <vasiffa@code.edu.az>
2020-01-23 08:09:49 -0800

    create controller

---
+++ Meverex/Controllers/NewsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Meverex.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Sports()
+        {
+            return View();
+        }
+        public ActionResult Fashion()
+        {
+            return View();
+        }
+        public ActionResult Travel()
+        {
+            return View();
+        }
+        public ActionResult Food()
+        {
+            return View();
+        }
+    }
+}
